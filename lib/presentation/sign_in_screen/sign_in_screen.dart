5126d4d744a2eacf3cec6151c60cbcbd931b1114
sarahturki <130418270+sarahturki@users.noreply.github.com>
2023-05-18 19:10:44 +0300

    second

--- lib/presentation/sign_in_screen/sign_in_screen.dart
+++ lib/presentation/sign_in_screen/sign_in_screen.dart
@@ -253,7 +253,7 @@ ScaffoldMessenger.of(context).showSnackBar( SnackBar( content: Text('كلمة ا
                       ),
                       validator: (value) {
                         if (value == null || value.isEmpty) {
-                              return 'الرجاء ادخال البريد الإلكتروني   ';
+                              return 'الرجاء ادخال البريد الإلكتروني   mcmmc ';
                         }
                         return null;
                       },
